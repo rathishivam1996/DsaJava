fd06d76db24e947f061f12473ef1f7800eac68bf
Shivam Rathi <shivamrathi2.sr@gmail.com>
2023-09-02 11:01:26 +0530

    Arrays

--- src/main/java/org/example/ArrayGfg.java
+++ src/main/java/org/example/ArrayGfg.java
@@ -17,6 +17,14 @@ public class ArrayGfg {
         list.add(8);
 
         System.out.println(maxMin(3, list));
+
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println("binary search: " + binarySearch(arr, 0, arr.length - 1, 6));
+
+        System.out.println("binary search: " + binarySearchFirstOcc(new int[]{1, 2, 2, 2, 3, 3, 4}, 0));
+
+        int[] arrRotated = new int[]{1, 2, 3, 0};
+        System.out.println("binary search rotated array: " + binarySearchRotated(arrRotated, 2));
     }
 
     public static int candies(int[] students) {
@@ -61,5 +69,74 @@ public class ArrayGfg {
         }
 
         return minDiff;
+    }
+
+    public static int binarySearch(int[] arr, int si, int ei, int key) {
+//        int si = 0;
+//        int ei = arr.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (key == arr[mid]) {
+                return mid;
+            } else if (key < arr[mid]) {
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    // O(log(n))
+    public static int binarySearchFirstOcc(int[] arr, int k) {
+        int si = 0;
+        int ei = arr.length - 1;
+        int res = -1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (k == arr[mid]) {
+                // found candidate
+                res = mid;
+                // keep on searching left
+                ei = mid - 1;
+            } else if (k < arr[mid]) {
+                ei = ei - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public static int binarySearchRotated(int[] arr, int target) {
+        int si = 0;
+        int ei = arr.length - 1;
+
+        // if (si == ei) return arr[si] == target ? si : -1;
+        // single element is always sorted, or if array is already sorted
+        if (arr[si] <= arr[ei]) return binarySearch(arr, si, ei, target);
+
+        int minIdx = 1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            int next = (mid + 1) % arr.length;
+            int prev = (mid + arr.length - 1) % arr.length;
+            if (arr[mid] < arr[prev] && arr[mid] < arr[next]) {
+                minIdx = mid;
+                break;
+            } else if (arr[mid] < arr[0]) {
+                ei = mid - 1;
+            } else if (arr[mid] > arr[arr.length - 1]) {
+                si = mid + 1;
+            }
+        }
+
+        int a = binarySearch(arr, 0, minIdx - 1, target);
+        int b = binarySearch(arr, minIdx, arr.length - 1, target);
+
+        return a == -1 ? b : a;
     }
 }
